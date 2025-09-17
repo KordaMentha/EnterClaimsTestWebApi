fba642202c975cc658ee9811ea5726737ee06b00
Samarth Nagaraj <licensing@kordamentha.com>
2025-09-17 13:12:58 +1000

    array value option updated

--- EnterClaimsTestWebApi/Controllers/TestController.cs
+++ EnterClaimsTestWebApi/Controllers/TestController.cs
@@ -10,7 +10,7 @@ namespace EnterClaimsTestWebApi.Controllers
         [HttpGet("SimpleTest")]
         public IActionResult SimpleTest()
         {
-            string[] testCodes = new string[] { "123456", "654321", "111222", "333444", "999999", "817543" };
+            string[] testCodes = new string[] { "123456", "654321", "111222", "333444", "999999", "1098765" };
             return Ok(testCodes);
         }
 
